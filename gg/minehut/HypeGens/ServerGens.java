a5d9d393982ff46183d52b2a59dab81fd38b8afd
Ding1367 <95048906+Ding1367@users.noreply.github.com>
2022-11-11 00:26:28 -0500

    Update ServerGens.java

--- gg/minehut/HypeGens/ServerGens.java
+++ gg/minehut/HypeGens/ServerGens.java
@@ -1,24 +1,26 @@
-package gg.minehut.HypeGen.commands;
+package gg.minehut.HypeGen;
 
-import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
-import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.java.JavaPlugin;
 
-import gg.minehut.HypeGen.ServerGens;
+import gg.minehut.HypeGen.commands.ReloadConfig;
 
-public class ReloadConfig implements CommandExecutor {
-	
-	private ServerGens plugin;
-	
-	public ReloadConfig(ServerGens plugin) {
-		this.plugin = plugin;
-	}
-	
+public class ServerGens extends JavaPlugin {
+
 	@Override
-	public boolean onCommand(CommandSender arg0, Command arg1, String arg2, String[] arg3) {
-		arg0.sendMessage("The configuration file has been reloaded.");
-		plugin.getLogger().finest("The configuration file has been reloaded with no errors.");
-		return false;
+	public void onEnable() {
+		saveDefaultConfig();
+		saveConfig();
+		registerCommand("reloadconfig", new ReloadConfig(this));
 	}
-	
+
+	@Override
+	public void onDisable() {
+
+	}
+
+	public void registerCommand(String name, CommandExecutor executor) {
+		getCommand(name).setExecutor(executor);
+	}
+
 }
